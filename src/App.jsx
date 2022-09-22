5e830417944b2db0358f913891fd2a108759397c
PerezFe <PerezFernandoE83@gmail.com>
2022-09-22 16:54:54 -0300

    agregamos react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Prueba</h1>
+      <h1 className='display-4'>Prueba</h1>
     </div>
   );
 }
