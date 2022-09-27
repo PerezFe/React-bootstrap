df0e2dafea33a98350e4e5b64d0658fe5cb6304d
PerezFe <PerezFernandoE83@gmail.com>
2022-09-27 18:15:15 -0300

    guardado en localStorage

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ListaTarea from "./ListaTarea";
 import { Form, Button } from "react-bootstrap";
 
+
+
+
+
 const FormularioTarea = () => {
+  //busco los datos del localstorage
+  const tareasLocalStorage = JSON.parse(localStorage.getItem("arregloTareaKey")) || [];
   // aqui va la  mayoria de la logica
   const [tarea, setTarea] = useState("");
-  const [arregloTarea, setArregloTarea] = useState([]);
+  const [arregloTarea, setArregloTarea] = useState(tareasLocalStorage);
+
+  //ciclo de vide del componente
+  useEffect(()=>{
+    //guardar en local storage
+    localStorage.setItem("arregloTareaKey", JSON.stringify(arregloTarea));
+  },[arregloTarea])
 
   const handleSubmit = (e) => {
     e.preventDefault();
